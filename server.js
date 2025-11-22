856f0b539f4db85ef84a05435020bcc6089812a9
Shailavi04 <shailavisrivastava977@gmail.com>
2025-11-22 16:14:24 +0530

    Push frontend

--- server.js
+++ server.js
@@ -91,8 +91,7 @@ app.delete("/students/:id", async (req, res) => {
 
  //Serve index.html for root
  app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/index.html"));
- });
+res.sendFile(path.join(__dirname, "frontend", "index.html")); });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
